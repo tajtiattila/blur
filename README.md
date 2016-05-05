45c2e5feeff8a1cc3bbf0acba19289f6f2a688af
Attila Tajti <attila.tajti@gmail.com>
2016-05-05 09:24:50 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+blur
+====
+
+Fast gaussian blur based on http://blog.ivank.net/fastest-gaussian-blur.html in [Go](http://golang.org).
+
+Example
+=======
+
+Original image
+
+![Original image](/sample/cballs.png)
+
+Blurred image
+
+![Blurred image](/sample/cballs_blur.png)
---
+++ blur_test.go
@@ -0,0 +1,35 @@
+package blur_test
+
+import (
+	"image"
+	"image/png"
+	"os"
+	"testing"
+
+	"github.com/tajtiattila/blur"
+)
+
+func TestBlur(t *testing.T) {
+	f, err := os.Open("sample/cballs.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	im, _, err := image.Decode(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dst := blur.Gaussian(im, 5)
+
+	f, err = os.Create("sample/cballs_blur.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	if err := png.Encode(f, dst); err != nil {
+		t.Fatal(err)
+	}
+}
